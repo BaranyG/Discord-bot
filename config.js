66d783390903a1ef3111ec084ab54c74745f370e
BaranyG <72973458+BaranyG@users.noreply.github.com>
2021-08-29 23:56:41 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    prefix: '!',
-    Token: 'NzMwMTQyNjM2MjI4NDc3MDE4.XwTMUQ.tN3S3S0dCP0x8ifD0SBreUzYNWU',
-    tesztToken: 'NzMwMTA4NjgyNzA4MTg5MjQ3.XwTLoQ.sTHp4ejC1ZKBH5lW80957Ggvcyw'
-}
