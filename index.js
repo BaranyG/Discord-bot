2554435adc57f0ae44c3b0d27f4b13bb41c32234
BaranyG <72973458+BaranyG@users.noreply.github.com>
2021-08-30 21:34:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord  = require('discord.js');       //Discord funkciók
 const client   = new Discord.Client({ 
 partials:["MESSAGE", "CHANNEL", "REACTION"]});//Discord kliens létrehozása
 module.exports = client;
-const config   = require('./config.js');
 const func     = require('./functions.js');   //Saját funkciók
 
 client.commands = new Discord.Collection();
@@ -12,4 +11,3 @@ client.events = new Discord.Collection();
 });
 
 client.login(require('./config.js').Token);  //BárányG Bot
-//Discord szín: #ff00e5
