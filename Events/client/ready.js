473d9742bf8329966ecdd7612aa65e49d8dfe2ec
BaranyG <72973458+BaranyG@users.noreply.github.com>
2021-08-31 18:05:11 +0200

    Add files via upload

--- Events/client/ready.js
+++ Events/client/ready.js
@@ -1,12 +1,15 @@
 module.exports = (Discord, client, func) => {
 
-
-    console.log("ez tok konnyu xd");
-    //client.user.setActivity(`Online ${client.guilds.cache.size} szerveren`);
     client.user.setActivity(`Veri a faszát ${client.guilds.cache.size} szerveren`);
-    console.log("\x1b[32m" + 'Birthday is online!' + "\x1b[0m");
+    console.log("\x1b[32m" + 'Iranai bot is online!' + "\x1b[0m");
     console.log("\x1b[32m" + `Logged in as ${client.user.tag}!` + "\x1b[0m");
-    console.log("\x1b[32m" + "BIOS Mai dátum:", func.BIOSdate()  + "\x1b[0m");        //lekérdezi mai dátumot BIOS-ból
+    //lekérdezi mai dátumot BIOS-ból
+    console.log("\x1b[32m" + "BIOS Mai dátum:", func.BIOSdate()  + "\x1b[0m");
+    //Lekérdezi a mai dátumot WorldTime API-ról.
+    func.WorldTime_API((err, date) => {
+        if(err) { console.log(err); return; }
+        console.log("\x1b[32m" + "API Mai dátum: " + date + "\x1b[0m");
+    });
         
     //let faszporgetok = client.guilds.cache.get(adatok.faszporgetok.id)
     //let baranyg = faszporgetok.members.cache.map(member => member.id);
@@ -17,7 +20,7 @@ module.exports = (Discord, client, func) => {
     //client.guilds.cache.get(adatok.unlimited.id).channels.cache.get(adatok.unlimited.tarsalgo).send(exampleEmbed);
     //client.guilds.cache.get(adatok.faszporgetok.id).channels.cache.get(adatok.faszporgetok.tarsalgo).send(emoji(adatok.emojik.feelsokayman) + emoji(adatok.emojik.bogre));
 
-    //serverCompare();
+    func.serverCompare();
+    func.roleAdd();
     func.httpCreateServer;
 }
-
