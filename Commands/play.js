0758979720c80878105362da49e833eb640d8e68
BaranyG <72973458+BaranyG@users.noreply.github.com>
2021-09-22 18:42:19 +0200

    Add files via upload

--- Commands/play.js
+++ Commands/play.js
@@ -1,10 +1,9 @@
-const { joinVoiceChannel, getVoiceConnection, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
 const ytdl = require("ytdl-core");
+const ytdl_discord= require("ytdl-core-discord");
 const ytSearch = require("yt-search");
 const ids = require("../ID.js");
 const player = createAudioPlayer();
-let resource = createAudioResource();
-resource.volume.setVolume(0.5);
 
 const queue = new Map();
 // queue(message.guild.id, queue_constructor object { voice channel, text channel, connection, song[]});
@@ -15,28 +14,28 @@ module.exports = {
     description: 'music bot',
     async execute(Discord, client, func, message, args, cmd){
 
-        const connection = joinVoiceChannel({
-            channelId: message.member.voice.channel.id,
-            guildId: message.guild.id,
-            adapterCreator: message.member.voice.channel.guild.voiceAdapterCreator,
-        });
-        connection.subscribe(player);
-
         const voice_channel = message.member.voice.channel;
         if(!voice_channel) return message.channel.send('Voice szobában kell lenned!');
         const permissions = voice_channel.permissionsFor(message.client.user);
-        if(!permissions.has("CONNECT")) return message.channel.send("Nincs megfelelő jogosultságod");
-        if(!permissions.has("SPEAK")) return message.channel.send("Nincs megfelelő jogosultságod");
+        if(!permissions.has("CONNECT")) return message.channel.send("Nincs megfelelő jogosultságod!");
+        if(!permissions.has("SPEAK")) return message.channel.send("Nincs megfelelő jogosultságod!");
 
+        //Ennek a helye még nem fix
+        const connection = joinVoiceChannel({
+            channelId: voice_channel,
+            guildId: message.guild.id,
+            adapterCreator: voice_channel.guild.voiceAdapterCreator,
+        });
+        connection.subscribe(player);
         const server_queue = queue.get(message.guild.id);
 
         if(cmd === "play"){
-            if(!args.length) return message.channel.send("Add meg a zene címét vagy linkjét.");
+            if(!args.length) return message.channel.send("Add meg a zene címét vagy linkjét!");
             let song = {};
 
             if(ytdl.validateURL(args[0])){
                 const song_info = await ytdl.getInfo(args[0]);
-                song = { title: song_info.videoDetails, url: song_info.videoDetails.video_url };
+                song = { title: song_info.videoDetails.title, url: song_info.videoDetails.video_url };
             } else {
                 //Ha nem linket írt be akkor használja a kulcsszavakat hogy megtalálja a videót
                 const video_finder = async (query) => {
@@ -48,7 +47,7 @@ module.exports = {
                 if(video){
                     song = { title: video.title, url: video.url }
                 } else {
-                    message.channel.send("Videó keresés hiba");
+                    message.channel.send("Ilyen nem is létezik.");
                 }
             }
             if(!server_queue){
@@ -64,17 +63,17 @@ module.exports = {
                 queue_constructor.songs.push(song);
     
                 try {
-                    //const connection = joinVoiceChannel(voice_channel);
+                    //const connection = joinVoiceChannel(voice_channel);   //Ezt itt hagyom emlékeztetőnek
                     queue_constructor.connection = connection;
                     video_player(message.guild, queue_constructor.songs[0]);
                 }catch(err){
                     queue.delete(message.guild.id);
-                    message.channel.send("Hiba lépett fel a felcsatlakozásban!")
+                    message.channel.send("Nem lehet felcsatlakozni!")
                     throw err;
                 }
             }else{
                 server_queue.songs.push(song);
-                return message.channel.send(func.emoji(ids.emojik.gabesz) + `**${song.title}** Hozzáadva a várólistához!`)
+                return message.channel.send(`Hozzáadva a várólistához: :musical_note: **${song.title}** :musical_note:`)
             }
         }
     }
@@ -88,21 +87,25 @@ const video_player = async (guild, song) => {
         queue.delete(guild.id);
         return;
     }
-    const stream = ytdl(song.url, { filter: "audioonly" });
+    const stream = ytdl(song.url, { highWaterMark: 1<<25, filter: "audioonly" });
+
+    let resource = createAudioResource(stream, { inputType: StreamType.OggOpus });
     song_queue.connection.subscribe(player);
     
-    player.play(stream, { seek: 0, volume: 0.5 })
-    .on("finish", () => {
+    player.play(resource);
+    
+    //Eventek:
+    player.on("finish", () => {
         song_queue.songs.shift();
         video_player(guild, song_queue.songs[0]);
     });
-    await song_queue.text_channel.send(`Most medzs **${song.title}**`);
+    player.on("error", error => {
+        console.error(error);
+    });
+    player.on(AudioPlayerStatus.Idle, () => {
+        song_queue.songs.shift();
+        video_player(guild, song_queue.songs[0]);
+    });
+
+    await song_queue.text_channel.send(`Most :cherries::  :musical_note: **${song.title}** :musical_note:`);
 }
-//
-//const stream = ytdl(song.url, { filter: "audioonly" });
-//    song_queue.connection.play(stream, { seek: 0, volume: 0.5 })
-//    .on("finish", () => {
-//        song_queue.songs.shift();
-//        video_player(guild, song_queue.songs[0]);
-//    });
-//    await song_queue.text_channel.send(`Most medzs **${song.title}**`);
