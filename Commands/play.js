cd86ae7e0190eb45d7aad8ce834acf73cd0c30e1
BaranyG <72973458+BaranyG@users.noreply.github.com>
2021-09-27 07:41:51 +0200

    Add files via upload

--- Commands/play.js
+++ Commands/play.js
@@ -1,9 +1,11 @@
-const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType, AudioPlayerStatus } = require('@discordjs/voice');
 const ytdl = require("ytdl-core");
-const ytdl_discord= require("ytdl-core-discord");
+const ytdlDiscord = require("ytdl-core-discord");
 const ytSearch = require("yt-search");
 const ids = require("../ID.js");
 const player = createAudioPlayer();
+const func = require('../functions.js');
 
 const queue = new Map();
 // queue(message.guild.id, queue_constructor object { voice channel, text channel, connection, song[]});
@@ -13,29 +15,23 @@ module.exports = {
     aliases: ['skip', 'stop'],
     description: 'music bot',
     async execute(Discord, client, func, message, args, cmd){
-
+        
         const voice_channel = message.member.voice.channel;
         if(!voice_channel) return message.channel.send('Voice szobában kell lenned!');
         const permissions = voice_channel.permissionsFor(message.client.user);
         if(!permissions.has("CONNECT")) return message.channel.send("Nincs megfelelő jogosultságod!");
         if(!permissions.has("SPEAK")) return message.channel.send("Nincs megfelelő jogosultságod!");
 
-        //Ennek a helye még nem fix
-        const connection = joinVoiceChannel({
-            channelId: voice_channel,
-            guildId: message.guild.id,
-            adapterCreator: voice_channel.guild.voiceAdapterCreator,
-        });
-        connection.subscribe(player);
         const server_queue = queue.get(message.guild.id);
 
         if(cmd === "play"){
             if(!args.length) return message.channel.send("Add meg a zene címét vagy linkjét!");
             let song = {};
-
+            // Link alapján megtalálja a videót youtube-on.
             if(ytdl.validateURL(args[0])){
                 const song_info = await ytdl.getInfo(args[0]);
                 song = { title: song_info.videoDetails.title, url: song_info.videoDetails.video_url };
+                
             } else {
                 //Ha nem linket írt be akkor használja a kulcsszavakat hogy megtalálja a videót
                 const video_finder = async (query) => {
@@ -63,7 +59,11 @@ module.exports = {
                 queue_constructor.songs.push(song);
     
                 try {
-                    //const connection = joinVoiceChannel(voice_channel);   //Ezt itt hagyom emlékeztetőnek
+                    const connection = joinVoiceChannel({
+                        channelId: voice_channel.id,
+                        guildId: message.guild.id,
+                        adapterCreator: voice_channel.guild.voiceAdapterCreator,
+                    });
                     queue_constructor.connection = connection;
                     video_player(message.guild, queue_constructor.songs[0]);
                 }catch(err){
@@ -80,31 +80,29 @@ module.exports = {
 }
 
 const video_player = async (guild, song) => {
+
     const song_queue = queue.get(guild.id);
 
-    if(!song){
-        song_queue.voice_channel.leave();
-        queue.delete(guild.id);
-        return;
-    }
     const stream = ytdl(song.url, { highWaterMark: 1<<25, filter: "audioonly" });
 
-    let resource = createAudioResource(stream, { inputType: StreamType.OggOpus });
+    let resource = createAudioResource(stream, { inlineVolume: false, });
+    
     song_queue.connection.subscribe(player);
     
     player.play(resource);
-    
-    //Eventek:
-    player.on("finish", () => {
-        song_queue.songs.shift();
-        video_player(guild, song_queue.songs[0]);
-    });
+
     player.on("error", error => {
         console.error(error);
     });
     player.on(AudioPlayerStatus.Idle, () => {
         song_queue.songs.shift();
-        video_player(guild, song_queue.songs[0]);
+        if(song_queue.songs.length)
+            video_player(guild, song_queue.songs[0]);
+        else if(song_queue.connection.state.status !== "destroyed"){
+            song_queue.connection.destroy();
+            queue.delete(guild.id);
+            return;
+        }
     });
 
     await song_queue.text_channel.send(`Most :cherries::  :musical_note: **${song.title}** :musical_note:`);
